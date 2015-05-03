708993837d5d1846d2f2989c3176d83785c87904
Johanna Cristine Dy <johannady2@gmail.com>
2015-05-03 17:07:55 +1100

    5-3-15 5:07pm
    solutions part 8

--- www/js/app.js
+++ www/js/app.js
@@ -2,70 +2,41 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
     var service = new EmployeeService();
     service.initialize().done(function () {
-        console.log("Service initialized");
-		renderHomeView();
+        renderHomeView();
     });
-	
-	var homeTpl = Handlebars.compile($("#home-tpl").html());
-	var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
 
     /* --------------------------------- Event Registration -------------------------------- */
-		//  $('.search-key').on('keyup', findByName);
-		/*  $('.help-btn').on('click', function() {
-			alert("Employee Directory v3.4");
-		});
-		*/
+    document.addEventListener('deviceready', function () {
+        StatusBar.overlaysWebView( false );
+        StatusBar.backgroundColorByHexString('#ffffff');
+        StatusBar.styleDefault();
+        FastClick.attach(document.body);
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }, false);
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
         service.findByName($('.search-key').val()).done(function (employees) {
-           /* var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-			*/
-			
-			 $('.content').html(employeeListTpl(employees));
+            $('.content').html(employeeListTpl(employees));
         });
     }
-	
-	function renderHomeView()
-	{
-		/*var html =
-		  "<h1>Directory</h1>" +
-		  "<input class='search-key' type='search' placeholder='Enter name'/>" +
-		  "<ul class='employee-list'></ul>";
-		$('body').html(html);
-		*/
-		
-		$('body').html(homeTpl());
-		$('.search-key').on('keyup', findByName);
-	}
+
+    function renderHomeView() {
+        $('body').html(homeTpl());
+        $('.search-key').on('keyup', findByName);
+    }
 
 }());
-
-
-
-document.addEventListener('deviceready', function () {
-	
-	StatusBar.overlaysWebView( false );
-	StatusBar.backgroundColorByHexString('#ffffff');
-	StatusBar.styleDefault();
-  if (navigator.notification) { // Override default HTML alert with native dialog
-      window.alert = function (message) {
-          navigator.notification.alert(
-              message,    // message
-              null,       // callback
-              "Workshop", // title
-              'OK'        // buttonName
-          );
-      };
-  }
-}, false);
-
-FastClick.attach(document.body);
